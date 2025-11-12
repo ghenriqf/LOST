7cb8ca3ef03384d5eadeea04ee75d878ddb39997
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-11-12 20:25:55 -0300

    feat: adicionar efeito parallax background

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private MeshRenderer _meshRenderer;
+    [SerializeField] private float animationSpeed;
+
+    private void Awake()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void Update()
+    {
+        _meshRenderer.material.mainTextureOffset += new Vector2(animationSpeed * Time.deltaTime, 0);
+    }
+}
