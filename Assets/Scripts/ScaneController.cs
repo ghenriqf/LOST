d8a68c029ba613640243f528382776d4489cadc0
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-11-17 22:23:46 -0300

    feat: adicionar cutscene inicial

--- Assets/Scripts/ScaneController.cs
+++ Assets/Scripts/ScaneController.cs
@@ -12,6 +12,7 @@ public class ScaneController : MonoBehaviour
     {
         parallax = FindObjectsOfType<Parallax>();
     }
+
     private IEnumerator BrakeCar(float time)
     {
         yield return new WaitForSeconds(time);
@@ -23,7 +24,74 @@ public class ScaneController : MonoBehaviour
             StartCoroutine(p.BreakParallaxSmooth());
         }
     }
-    
+
+    public SpriteRenderer[] sprites;
+    public SpriteRenderer blackBackground;
+
+    public float fadeDuration = 1f;
+    public float timeVisible = 1f;
+
+    IEnumerator PlaySequence()
+    {
+        SetAlpha(blackBackground, 0);
+
+        for (int i = 0; i < sprites.Length; i++)
+        {
+            SpriteRenderer sr = sprites[i];
+
+            yield return StartCoroutine(FadeIn(sr, fadeDuration));
+            yield return new WaitForSeconds(timeVisible);
+
+            SetAlpha(blackBackground, 1);
+
+            yield return StartCoroutine(FadeOut(sr, fadeDuration));
+
+            if (i == sprites.Length - 1)
+            {
+                yield return StartCoroutine(FadeOut(blackBackground, fadeDuration));
+            }
+        }
+    }
+
+    IEnumerator FadeIn(SpriteRenderer sr, float duration)
+    {
+        float t = 0;
+        Color c = sr.color;
+        c.a = 0;
+        sr.color = c;
+
+        while (t < duration)
+        {
+            t += Time.deltaTime;
+            c.a = Mathf.Lerp(0, 1, t / duration);
+            sr.color = c;
+            yield return null;
+        }
+    }
+
+    IEnumerator FadeOut(SpriteRenderer sr, float duration)
+    {
+        float t = 0;
+        Color c = sr.color;
+        c.a = 1;
+        sr.color = c;
+
+        while (t < duration)
+        {
+            t += Time.deltaTime;
+            c.a = Mathf.Lerp(1, 0, t / duration);
+            sr.color = c;
+            yield return null;
+        }
+    }
+
+    void SetAlpha(SpriteRenderer sr, float a)
+    {
+        Color c = sr.color;
+        c.a = a;
+        sr.color = c;
+    }
+
     public void ResetScene()
     {
         car.GetComponent<Animator>().enabled = true;
@@ -32,12 +100,21 @@ public class ScaneController : MonoBehaviour
             p.ResetParallaxInstant();
         }
     }
-    
+
     void Start()
     {
         StartCoroutine(BrakeCar(10));
+
+        foreach (var sr in sprites)
+        {
+            SetAlpha(sr, 0);
+        }
+
+        SetAlpha(blackBackground, 0);
+
+        StartCoroutine(PlaySequence());
     }
-    
+
     void Update()
     {
         if (Keyboard.current.rKey.wasPressedThisFrame)
