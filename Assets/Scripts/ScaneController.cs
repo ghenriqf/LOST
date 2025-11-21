4f21892bb93ceeb3274350527a5cf7f064708baa
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-11-20 22:31:30 -0300

    feat: continuar cena cutscene inicial

--- Assets/Scripts/ScaneController.cs
+++ Assets/Scripts/ScaneController.cs
@@ -9,28 +9,52 @@ public class ScaneController : MonoBehaviour
     private Parallax[] parallax;
     public SpriteRenderer[] sprites;
     public SpriteRenderer blackBackground;
+    public Transform house;
 
+    [Header("Tempos da Cutscene")]
     public float fadeDuration = 1f;
     public float timeVisible = 1f;
+    public float parallaxDuration = 10f; 
+    public float houseMoveDuration = 3f;
+
+    [Header("Posições da Casa")]
+    public float houseEndX = 0.8f; 
+    private Vector3 houseStartPos;
 
     void Awake()
     {
         parallax = FindObjectsOfType<Parallax>();
+        houseStartPos = new Vector3(1.858f, -0.174f, -1.25f);
+        house.position = houseStartPos;
     }
 
-    private IEnumerator BrakeCar(float time)
+    void Start()
     {
-        yield return new WaitForSeconds(time);
-        car.GetComponent<Animator>().enabled = false;
-        audioSource.Play();
+        foreach (var sr in sprites)
+            SetAlpha(sr, 0);
+        SetAlpha(blackBackground, 0);
 
         foreach (var p in parallax)
-        {
-            StartCoroutine(p.BreakParallaxSmooth());
-        }
+            p.enabled = true;
+
+        StartCoroutine(CutsceneSequence());
     }
 
-    IEnumerator PlaySequence()
+    IEnumerator CutsceneSequence()
+    {
+        yield return new WaitForSeconds(parallaxDuration);
+
+        yield return StartCoroutine(PlayImageSequence());
+
+        yield return new WaitForSeconds(parallaxDuration);
+
+        yield return StartCoroutine(MoveHouseX(houseStartPos.x, houseEndX, houseMoveDuration));
+
+        foreach (var p in parallax)
+            p.enabled = false;
+    }
+
+    IEnumerator PlayImageSequence()
     {
         SetAlpha(blackBackground, 0);
 
@@ -40,9 +64,7 @@ public class ScaneController : MonoBehaviour
 
             yield return StartCoroutine(FadeIn(sr, fadeDuration));
             yield return new WaitForSeconds(timeVisible);
-
             SetAlpha(blackBackground, 1);
-
             yield return StartCoroutine(FadeOut(sr, fadeDuration));
 
             if (i == sprites.Length - 1)
@@ -50,6 +72,22 @@ public class ScaneController : MonoBehaviour
                 yield return StartCoroutine(FadeOut(blackBackground, fadeDuration));
             }
         }
+    }
+
+    IEnumerator MoveHouseX(float startX, float endX, float duration)
+    {
+        float t = 0;
+        Vector3 currentPos = house.position;
+
+        while (t < duration)
+        {
+            t += Time.deltaTime;
+            float newX = Mathf.Lerp(startX, endX, t / duration);
+            house.position = new Vector3(newX, currentPos.y, currentPos.z);
+            yield return null;
+        }
+
+        house.position = new Vector3(endX, currentPos.y, currentPos.z);
     }
 
     IEnumerator FadeIn(SpriteRenderer sr, float duration)
@@ -94,24 +132,19 @@ public class ScaneController : MonoBehaviour
     public void ResetScene()
     {
         car.GetComponent<Animator>().enabled = true;
+
         foreach (var p in parallax)
         {
             p.ResetParallaxInstant();
+            p.enabled = true;
         }
-    }
-
-    void Start()
-    {
-        StartCoroutine(BrakeCar(10));
 
         foreach (var sr in sprites)
-        {
             SetAlpha(sr, 0);
-        }
 
         SetAlpha(blackBackground, 0);
 
-        StartCoroutine(PlaySequence());
+        house.position = houseStartPos;
     }
 
     void Update()
