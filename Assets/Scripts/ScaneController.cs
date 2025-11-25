ea69167390ce72f95c2a4fc096d1b07814337c94
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-11-24 21:28:11 -0300

    feat: continuar cena inicial

--- Assets/Scripts/ScaneController.cs
+++ Assets/Scripts/ScaneController.cs
@@ -1,30 +1,35 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class ScaneController : MonoBehaviour
 {
     [SerializeField] private GameObject car;
     [SerializeField] private AudioSource audioSource;
+
     private Parallax[] parallax;
+
     public SpriteRenderer[] sprites;
     public SpriteRenderer blackBackground;
     public Transform house;
 
-    [Header("Tempos da Cutscene")]
     public float fadeDuration = 1f;
     public float timeVisible = 1f;
-    public float parallaxDuration = 10f; 
+    public float parallaxDuration = 10f;
     public float houseMoveDuration = 3f;
 
-    [Header("Posições da Casa")]
-    public float houseEndX = 0.8f; 
+    public float houseEndX = 0.8f;
     private Vector3 houseStartPos;
+
+    public TextMeshProUGUI dialogueText;
+    public float dialogueFadeDuration = 0.5f;
+    public float typeSpeed = 0.04f;
 
     void Awake()
     {
         parallax = FindObjectsOfType<Parallax>();
-        houseStartPos = new Vector3(1.858f, -0.174f, -1.25f);
+        houseStartPos = new Vector3(1.98f, -0.28f, -1.25f);
         house.position = houseStartPos;
     }
 
@@ -32,22 +37,36 @@ public class ScaneController : MonoBehaviour
     {
         foreach (var sr in sprites)
             SetAlpha(sr, 0);
+
         SetAlpha(blackBackground, 0);
 
         foreach (var p in parallax)
             p.enabled = true;
+
+        Color c = dialogueText.color;
+        c.a = 0;
+        dialogueText.color = c;
 
         StartCoroutine(CutsceneSequence());
     }
 
     IEnumerator CutsceneSequence()
     {
-        yield return new WaitForSeconds(parallaxDuration);
+        yield return StartCoroutine(ShowDialogueTypewriter("No interior do Texas, um jovem detetive dirigia, a procura do hotel para passar a sua noite.\n"));
+        yield return new WaitForSeconds(2f);
+        yield return StartCoroutine(HideDialogue());
 
+        yield return new WaitForSeconds(parallaxDuration);
+        yield return StartCoroutine(ShowDialogueTypewriter("Já era de madrugada. Para melhorar, a bússola andava oscilando, incapaz de apontar qualquer direção estável. E esse mapa, pelo amor de Deus, ele não ajudava nem um pouco.\n"));
+        yield return new WaitForSeconds(3f);
+        yield return StartCoroutine(HideDialogue());
         yield return StartCoroutine(PlayImageSequence());
 
         yield return new WaitForSeconds(parallaxDuration);
 
+        yield return StartCoroutine(ShowDialogueTypewriter("Em sua ardente busca, nota uma casa ao fundo da planície escura."));
+        yield return new WaitForSeconds(3f);
+        yield return StartCoroutine(HideDialogue());
         yield return StartCoroutine(MoveHouseX(houseStartPos.x, houseEndX, houseMoveDuration));
 
         foreach (var p in parallax)
@@ -129,6 +148,52 @@ public class ScaneController : MonoBehaviour
         sr.color = c;
     }
 
+    IEnumerator ShowDialogueTypewriter(string text)
+    {
+        dialogueText.text = "";
+
+        float t = 0;
+        Color c = dialogueText.color;
+        c.a = 0;
+        dialogueText.color = c;
+
+        while (t < dialogueFadeDuration)
+        {
+            t += Time.deltaTime;
+            c.a = Mathf.Lerp(0, 1, t / dialogueFadeDuration);
+            dialogueText.color = c;
+            yield return null;
+        }
+
+        yield return StartCoroutine(Typewriter(text));
+    }
+
+    IEnumerator Typewriter(string text)
+    {
+        dialogueText.text = "";
+
+        foreach (char letter in text.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(typeSpeed);
+        }
+    }
+
+    IEnumerator HideDialogue()
+    {
+        float t = 0;
+        Color c = dialogueText.color;
+        c.a = 1;
+
+        while (t < dialogueFadeDuration)
+        {
+            t += Time.deltaTime;
+            c.a = Mathf.Lerp(1, 0, t / dialogueFadeDuration);
+            dialogueText.color = c;
+            yield return null;
+        }
+    }
+
     public void ResetScene()
     {
         car.GetComponent<Animator>().enabled = true;
@@ -144,14 +209,10 @@ public class ScaneController : MonoBehaviour
 
         SetAlpha(blackBackground, 0);
 
-        house.position = houseStartPos;
-    }
+        Color c = dialogueText.color;
+        c.a = 0;
+        dialogueText.color = c;
 
-    void Update()
-    {
-        if (Keyboard.current.rKey.wasPressedThisFrame)
-        {
-            ResetScene();
-        }
+        house.position = houseStartPos;
     }
 }
