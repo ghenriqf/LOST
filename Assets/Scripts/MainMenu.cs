579ee6641dda04432ba54ae71eee8e88dd8250bb
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-12-04 13:37:43 -0300

    refactor: organizar cenas e menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("Scene1");
+        SceneManager.LoadScene("CarroCutscene");
     }
 
     public void QuitGame()
