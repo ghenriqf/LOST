23a4a7ade6ffeaf2aa8ad667639d0a745ae7429c
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-12-04 01:28:47 -0300

    feat: adicionar timer na cena porão

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public float tempo = 60f;
+    public TextMeshProUGUI texto;
+
+    void Update()
+    {
+        tempo -= Time.deltaTime;
+
+        if (tempo <= 0)
+        {
+            tempo = 0;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        texto.text = Mathf.CeilToInt(tempo).ToString();
+    }
+}
